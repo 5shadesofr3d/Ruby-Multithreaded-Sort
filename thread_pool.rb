848823a1140508b1139776648a2ca268c38d0177
curtis goud <cgoud@ualberta.ca>
2019-03-07 11:18:28 -0700

    some assertions and docs

--- thread_pool.rb
+++ thread_pool.rb
@@ -4,8 +4,10 @@ require 'concurrent-ruby'
 
 #Requires concurrent-ruby gem
 class ThreadPool
+  include Test::Unit::Assertions
+
   def initialize(numThreads)
-    #assert numThreads > 0
+    assert numThreads > 0
     @max_threads = numThreads
     @pool = Concurrent::FixedThreadPool.new(5)
 
@@ -15,37 +17,48 @@ class ThreadPool
 
 
   def work(&task)
+    assert task.is_a? Proc
     @pool.post do
-      task.call()
+      task.call
     end
   end
 
+  #Begins shutdown and cleanup of threads
+  #NOTE: the below shutdown will make all tasks complete first before shutdown.
   def shutdown
-    #NOTE: the below shutdown will make all tasks complete first before shutdown.
     @pool.shutdown
   end
 
+  #returns TRUE if the thread pool is running and ready to go
   def running?
     return @pool.running?
   end
 
+  #returns TRUE if done shutdown, false otherwise
   def shutdown?
     return @pool.shutdown?
   end
 
+  #returns TRUE if we are currently shutting down
   def shuttingdown?
-    return @pool.shutdown?
+    return @pool.shuttingdown?
   end
 
+  #Call this after shutdown to wait for shutdown to complete
   def wait_for_termination
-    #Call this after shutdown to wait for shutdown to complete
+    #pre
+    assert shuttingdown?
+
     @pool.wait_for_termination
+
+    #post
+    assert shutdown?
   end
 end
 
 #Small test:
-#myPool = ThreadPool.new(5)
-#myPool.work { puts "Test1"}
-#myPool.work { puts "Test2"}
-#myPool.shutdown
-#myPool.wait_for_termination
+myPool = ThreadPool.new(5)
+myPool.work { puts "Test1"}
+myPool.work { puts "Test2"}
+myPool.shutdown
+myPool.wait_for_termination
