dd8352d62d38d6804d9684b28c1a8ea58a12f19e
Jori Romans <jromans@ualberta.ca>
2019-03-07 16:56:19 -0700

    Revised initial code for mergeSort

--- merge.rb
+++ merge.rb
@@ -4,13 +4,13 @@ class Merge
   end
 
   def mergeSort(array, beg, end)
-    if beg != end
+    if beg == end
+      return array
+    else
       mid = (beg + end / 2)
       leftArray = mergeSort(array, beg, mid)
       rightArray = mergeSort(array, mid + 1, end)
       return merge(leftArray, rightArray)
-    else
-      return array
     end
   end
 end
